887c4cf5a94d4f417829bfe734432ba0a1db1d27
fdonzello <francesco.donzello@gmail.com>
2021-11-03 11:39:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-szquqb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-szquqb)
---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { TestComponent } from '../test/test.component';
+import { SecondTestComponent } from '../second-test/second-test.component';
+
+@NgModule({
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    TestComponent,
+    SecondTestComponent,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
