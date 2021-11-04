053a1dab5b46bea743ff2374efc855ba4e901f65
fdonzello <francesco.donzello@gmail.com>
2021-11-04 17:36:19 +0100

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
-import { TestComponent } from '../test/test.component';
-import { SecondTestComponent } from '../second-test/second-test.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from '../login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HelloComponent,
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+];
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [
-    AppComponent,
-    HelloComponent,
-    TestComponent,
-    SecondTestComponent,
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes),
   ],
+  declarations: [AppComponent, HelloComponent, LoginComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/post.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'post',
+})
+export class PostPipe implements PipeTransform {
+  transform(value: any, args?: any): any {
+    return 'id - title';
+  }
+}
